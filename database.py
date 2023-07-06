92ebe75edbe25e2cf68e92f3064b623add53c39f
Nikyxa <kolisova.veronika81@gmail.com>
2023-07-06 14:20:51 +0300

    Edit parser, create the logic for notifications

--- database.py
+++ database.py
@@ -6,12 +6,14 @@ c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS cars
              (id INTEGER PRIMARY KEY AUTOINCREMENT,
               model TEXT,
-              price INTEGER,
+              price INTEGER DEFAULT 0,
               mileage TEXT,
               ria_url TEXT,
+              page_id TEXT,
               photo_ria TEXT,
               auction_url TEXT,
-              is_sold INTEGER DEFAULT 0)''')
+              is_sold INTEGER DEFAULT 0,
+              is_sent INTEGER DEFAULT 0)''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS price_history
              (id INTEGER PRIMARY KEY AUTOINCREMENT,
--- parser.py
+++ parser.py
@@ -1,24 +1,19 @@
 import sqlite3
+import time
 from dataclasses import dataclass
 from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup
 
+from bot import update_price_message, send_message_to_channel
+
 BASE_URl = "https://auto.ria.com/uk/"
 CARS_URL = urljoin(BASE_URl, "search/?categories.main.id=1&brand.id[0]=79&model.id[0]=2104&indexName=auto,order_auto,newauto_search&damage.not=0&country.import.usa.not=0&size=100")
 
 
-@dataclass
-class Car:
-    car_id: int
-    model: str
-    price: float
-    mileage: str
-    ria_url: str
-    photo_ria: list[str]
-    auction_url: str
-    is_sold: bool
+conn = sqlite3.connect("car_data.db")
+cursor = conn.cursor()
 
 
 def parse_cars_urls():
@@ -39,19 +34,39 @@ def parse_one_page(url):
     return page_soup
 
 
-def insert_car_data(model, price, mileage, ria_url, photo_ria, auction_url, is_sold):
+def insert_car_data(model, price, mileage, ria_url, page_id, photo_ria, auction_url, is_sold):
     conn = sqlite3.connect("car_data.db")
     cursor = conn.cursor()
 
     insert_query = '''
-    INSERT INTO cars (model, price, mileage, ria_url, photo_ria, auction_url, is_sold)
-    VALUES (?, ?, ?, ?, ?, ?, ?)
+    INSERT INTO cars (model, price, mileage, ria_url, page_id, photo_ria, auction_url, is_sold)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
     '''
-    record_data = (model, price, mileage, ria_url, ",".join(photo_ria), auction_url, is_sold)
+    record_data = (model, price, mileage, ria_url, page_id, ",".join(photo_ria), auction_url, is_sold)
     cursor.execute(insert_query, record_data)
 
     conn.commit()
     conn.close()
+
+def check_unique_cars(model, price, mileage, page_id, ria_url, auction_url):
+    conn = sqlite3.connect("car_data.db")
+    cursor = conn.cursor()
+
+    select_query = "SELECT * FROM cars WHERE model = ? AND price = ? AND mileage = ? AND page_id = ? AND ria_url = ? AND auction_url = ?"
+    cursor.execute(select_query, (model, price, mileage, page_id, ria_url, auction_url))
+    existing_car = cursor.fetchone()
+
+    return existing_car
+
+
+def retrieve_last_price(page_id):
+    select_query = "SELECT price FROM cars WHERE page_id = ?"
+    cursor.execute(select_query, (page_id,))
+    result = cursor.fetchone()
+    if result is not None:
+        return result[0]
+    else:
+        return None
 
 
 def parse_one_car():
@@ -65,9 +80,9 @@ def parse_one_car():
         mileage = parse_url.find('span', class_='size18').text.strip()
         ria_url = car_url
 
-        # page_url_parts = car_url.split("_")
-        # url_part = page_url_parts[3].split(".")
-        # page_id = url_part[0]
+        page_url_parts = car_url.split("_")
+        url_part = page_url_parts[3].split(".")
+        page_id = url_part[0]
 
         photos = parse_url.find_all("img", class_="outline m-auto")
         photo_links = [photo['src'] for photo in photos]
@@ -79,11 +94,22 @@ def parse_one_car():
 
         is_sold = bool(parse_url.find("div", class_="gallery-order sold-out carousel"))
 
-        insert_car_data(model, price, mileage, ria_url, photo_ria, auction_url, is_sold)
-def get_car_urls(page):
-    car_urls = page.find_all('div', class_='content-bar')
-    return [car_url for car_url in car_urls]
+        if not check_unique_cars(model, price, mileage, page_id, ria_url, auction_url):
+            last_price = retrieve_last_price(page_id)
+            insert_car_data(model, price, mileage, ria_url, page_id, photo_ria, auction_url, is_sold)
+
+            if last_price is None:
+                continue
+            if last_price != price:
+                update_price_message(model, price, last_price)
 
 
+def main_scraper():
+    while True:
+        parse_one_car()
+        send_message_to_channel()
+        time.sleep(10 * 60)
 
-parse_one_car()
+
+if __name__ == '__main__':
+    main_scraper()
